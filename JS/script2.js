35fd808beb3f3075d171f3eed2d961a54281c330
EnriqueGimeno1 <enriquejgimeno@gmail.com>
2022-07-25 05:47:18 -0300

    Comentar Switch de Script2

--- JS/script2.js
+++ JS/script2.js
@@ -133,23 +133,23 @@
 
 // EJERCICIO 6.2 es igual al de arriba pero otro metodo
 
-switch (dia) {
-    case 1: console.log("Domingo");
-        break;
-    case 2: console.log("Lunes");
-        break;
-    case 3: console.log("Martes");
-        break;
-    case 4: console.log("Miercoles");
-        break;
-    case 5: console.log("Jueves");
-        break;
-    case 6: console.log("Viernes");
-        break;
-    case 7: console.log("Sabado");
-        break;
-    default: console.log("Numero Incorrecto");
-}
+// switch (dia) {
+//     case 1: console.log("Domingo");
+//         break;
+//     case 2: console.log("Lunes");
+//         break;
+//     case 3: console.log("Martes");
+//         break;
+//     case 4: console.log("Miercoles");
+//         break;
+//     case 5: console.log("Jueves");
+//         break;
+//     case 6: console.log("Viernes");
+//         break;
+//     case 7: console.log("Sabado");
+//         break;
+//     default: console.log("Numero Incorrecto");
+// }
 
 // CONDICIONALES DE DIFERENTES TIPOS EN UN IF
 
